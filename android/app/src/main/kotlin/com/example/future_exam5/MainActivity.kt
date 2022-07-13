6c324856710578f98818936da3e8c74e21942c8a
fulmoon <fulmoon@gmail.com>
2022-07-13 19:30:13 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/future_exam5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.future_exam5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var Greatman = ['이순신', '세종대왕', '광개토대왕', '다빈치', '피타고라스', '다윈', '에디슨', '아인슈타인', '링컨', '튜링', ];
+  int index = 0;
+  String pastman = '';
+
+  String exam5() {
+    setState(() {
+      index++;
+    });
+    String result = Greatman[index];
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '당신 전생은?',
+            ),
+            Text(
+              '$pastman',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+                onPressed: exam5,
+                child: const Text('Future 연습5'),
+            )
+          ]
+        ),
+      ),
+
+    );
+  }
+}
